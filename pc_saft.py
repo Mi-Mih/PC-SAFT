d450a44523d8ccb16b47539c1aa2648c3d100347
Mi-Mih <miham1195@gmail.com>
2023-01-06 22:02:00 +0300

    add launch method

--- pc_saft.py
+++ pc_saft.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from math import pi
 import logging
+import random
 
 
 class PCSAFT:
@@ -13,7 +14,7 @@ class PCSAFT:
         :param m: массив кол-ва сегментов в молекуле компонента
         :param rho: плотность
         :param sigma: массив диаметров сегментов
-        :param eps: массив энергетических параметров сегментов
+        :param eps: массив энергетических параметров сегментов/ постоянная Больцмана
         :param boltzmann: постоянная Больцмана
         :param mean_m: усреднённое число сегментов
         :param eta: Packing fraction
@@ -45,14 +46,14 @@ class PCSAFT:
 
     # метод получения коэффициентов a и b - протестировано
     def transfom_coefs(self, coefs_matrix: pd.DataFrame) -> np.array:
-        self.logger.debug(f' а_i and b_i calculation started')
+        # self.logger.debug(f' а_i and b_i calculation started')
         column = coefs_matrix.columns
         return (coefs_matrix[column[0]] + (self.mean_m - 1) / self.mean_m * coefs_matrix[column[1]] + (
                 (self.mean_m - 1) * (self.mean_m - 2)) / self.mean_m ** 2 * coefs_matrix[column[2]]).to_numpy()
 
     # метод расчёта интегралов возмущения - протестировано
     def calc_integral(self, coefs: np.array):
-        self.logger.debug(f' integral calculation started')
+        # self.logger.debug(f' integral calculation started')
         array_I = np.array([])
         for i in range(len(coefs)):
             array_I = np.append(array_I, coefs[i] * self.eta ** i)
@@ -60,29 +61,29 @@ class PCSAFT:
 
     # метод расчёта кси(0,1,2,3) - протестировано
     def ksi(self, n: int) -> float:
-        self.logger.debug(f' ksi_{n} calculation started')
+        # self.logger.debug(f' ksi_{n} calculation started')
         return (pi / 6) * self.rho * np.sum(np.array(self.x) * np.array(self.m) * np.array([x ** n for x in self.d]))
 
     # метод комбинированияв смесях - протестировано
     def comb_sigma(self, i: int, j: int) -> float:
-        self.logger.debug(f' sigma_{i, j} calculation started')
+        # self.logger.debug(f' sigma_{i, j} calculation started')
         return 0.5 * (self.sigma[i] + self.sigma[j])
 
     # метод комбинирования в смесях - протестировано
     def comb_eps(self, i: int, j: int) -> float:
-        self.logger.debug(f' epsilon_{i, j} calculation started')
+        # self.logger.debug(f' epsilon_{i, j} calculation started')
         return (1 - self.k[j]) * (self.eps[i] * self.eps[j]) ** 0.5
 
     # метод расчёта сжимаемости - протестировано
     def calc_c(self) -> float:
-        self.logger.debug(f' C calculation started')
+        # self.logger.debug(f' C calculation started')
         return (1 + self.mean_m * ((8 * self.eta - 2 * self.eta ** 2) / (1 - self.eta) ** 4) + (1 - self.mean_m) * (
                 20 * self.eta - 27 * self.eta ** 2 + 12 * self.eta ** 3 - 2 * self.eta ** 4) / (
                         (1 - self.eta) * (2 - self.eta)) ** 2) ** (-1)
 
     # метод расчёта m2_eps2_sigma3 - протестировано
     def calc_m2_eps2_sigma3(self) -> float:
-        self.logger.debug(f' m^2 * eps^2 * sigma^3 calculation started')
+        # self.logger.debug(f' m^2 * eps^2 * sigma^3 calculation started')
         m2_eps2_sigma3 = 0
         for i in range(len(self.x)):
             for j in range(len(self.x)):
@@ -92,7 +93,7 @@ class PCSAFT:
 
     # метод расчёта m2_eps_sigma3 - протестировано
     def calc_m2_eps_sigma3(self) -> float:
-        self.logger.debug(f' m^2 * eps * sigma^3 calculation started')
+        # self.logger.debug(f' m^2 * eps * sigma^3 calculation started')
         m2_eps_sigma3 = 0
         for i in range(len(self.x)):
             for j in range(len(self.x)):
@@ -102,7 +103,7 @@ class PCSAFT:
 
     # метод расчёта остаточной энергии Гельмгольца дисперсионных сил - протестировано
     def calc_alpha_disp(self):
-        self.logger.debug(f' dispersion alpha calculation started')
+        # self.logger.debug(f' dispersion alpha calculation started')
         a = self.transfom_coefs(pd.read_excel('a-b.xlsx'))
         b = self.transfom_coefs(pd.read_excel('a-b.xlsx'))
         I_1 = self.calc_integral(a)
@@ -115,7 +116,7 @@ class PCSAFT:
 
     # метод расчёта ост энергии Гельмгольца твёрдых сфер - протестировано
     def calc_alpha_hs(self):
-        self.logger.debug(f' alpha_hs calculation started')
+        # self.logger.debug(f' alpha_hs calculation started')
         ksi0, ksi1 = self.ksi(0), self.ksi(1)
         ksi2, ksi3 = self.ksi(2), self.ksi(3)
         return (1 / self.ksi(0)) * (
@@ -124,7 +125,7 @@ class PCSAFT:
 
     # метод радиальная функция распределения в системе твёрдых сфер - протестировано
     def radial_func_distr(self):
-        self.logger.debug(f' radial function distribution calculation started')
+        # self.logger.debug(f' radial function distribution calculation started')
         ksi2, ksi3 = self.ksi(2), self.ksi(3)
         g = np.zeros((len(self.x), len(self.x)))
         for i in range(g.shape[0]):
@@ -136,19 +137,19 @@ class PCSAFT:
 
     # метод расчёта ост энергии Гельмгольца твёрдых цепей - протестировано
     def calc_alpha_chain(self):
-        self.logger.debug(f' alpha hard chain calculation started')
+        # self.logger.debug(f' alpha hard chain calculation started')
         alpha_hard_sphere = self.calc_alpha_hs()
         g = self.radial_func_distr()
         return self.mean_m * alpha_hard_sphere - np.sum(self.x * (np.array(self.m) - 1) * np.log(g.diagonal()))
 
     # метод расчёта остаточной энергии Гельмгольца - протестировано
     def calc_energy_helmholtz(self):
-        self.logger.debug(f' energy of helmholtz calculation started')
+        # self.logger.debug(f' energy of helmholtz calculation started')
         return self.calc_alpha_chain() + self.calc_alpha_disp()
 
     # метод расчёта коэффициента сжимаемости - протестирован
     def calc_z(self):
-        self.logger.debug(f' z calculation started')
+        # self.logger.debug(f' z calculation started')
         rho_1 = self.rho
         eta_array = np.array([])
         for i in [-2, -1, 1, 2]:
@@ -167,13 +168,13 @@ class PCSAFT:
 
     # метод расчёта давления - протестирован
     def calc_pressure(self):
-        self.logger.debug(f' pressure calculation started')
+        # self.logger.debug(f' pressure calculation started')
         z = self.calc_z()
         self.pressure = z * self.boltzmann * self.temperature * self.rho * 10e24
 
     # метод расчёта частных производных - протестирован
     def calc_partial_x(self):
-        self.logger.debug(f' partials x calculation started')
+        # self.logger.debug(f' partials x calculation started')
         x_1 = self.x
         partial_array = np.array([])
         for i in range(len(self.x)):
@@ -191,7 +192,7 @@ class PCSAFT:
 
     # метод расчёта коэффициентов летучести - протестирован
     def calc_fugacity_coeff(self):
-        self.logger.debug(f' fugacity coeffs calculation started')
+        # self.logger.debug(f' fugacity coeffs calculation started')
         alpha_res = self.calc_energy_helmholtz()
         z = self.calc_z()
         partials = self.calc_partial_x()
@@ -205,8 +206,45 @@ class PCSAFT:
     # current - текущая фаза, forecast - неизвестная фаза
     # меняем плотность, молярный объём и eta
     def calc_lv_proportion(self, proportion, fugacity_current, fugacity_forecast):
-        self.logger.debug(f' vapour-liquid calculation started')
+        # self.logger.debug(f' vapour-liquid calculation started')
         return (proportion * fugacity_current) / fugacity_forecast
+
+
+# метод расчёта парожидкостного равновесия
+def launch_pcsaft(state_1: dict, state_2: dict, eps=1e-3, current_state='liquid'):
+    dicts_correct = {'cur_eta': [], 'forec_eta': [], 'error': [], 'cur_x': [], 'forec_x': []}
+
+    current = PCSAFT(**state_1)
+    forecast = PCSAFT(**state_2)
+
+    for eta_forec in [0.05, 0.07]: # доделать
+        forecast.eta = eta_forec
+        forecast.rho = 6 * eta_forec / np.sum(
+            np.array(current.x) * np.array(current.m) * np.array([x ** 3 for x in forecast.d]))
+        for eta_cur in [0.000003, 0.00000008]: # доделать
+            current.eta = eta_cur
+            current.rho = 6 * eta_cur / np.sum(
+                np.array(forecast.x) * np.array(forecast.m) * np.array([x ** 3 for x in current.d]))
+            current.calc_pressure()
+            forecast.calc_pressure()
+
+            current.calc_fugacity_coeff()
+            forecast.calc_fugacity_coeff()
+
+            forecast.x = forecast.calc_lv_proportion(current.x, current.fugacity_coeffs, forecast.fugacity_coeffs)
+            if len(forecast.x) != len([y for y in forecast.x if y==y]):
+                continue
+
+            x_scaled = [mol / sum(forecast.x) for mol in forecast.x]
+            forecast.x = x_scaled
+            dicts_correct['cur_eta'].append(eta_cur)
+            dicts_correct['forec_eta'].append(eta_forec)
+            dicts_correct['error'].append(abs(current.pressure - forecast.pressure))
+            dicts_correct['cur_x'].append(current.x)
+            dicts_correct['forec_x'].append(x_scaled)
+
+    df_correct = pd.DataFrame(dicts_correct)
+    return df_correct
 
 
 if __name__ == '__main__':
