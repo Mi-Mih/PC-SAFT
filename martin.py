76d1c97fd492946b0562b14c0caa2fa75c9119c3
Mi-Mih <92208626+Mi-Mih@users.noreply.github.com>
2023-01-31 21:30:30 +0300

    Update martin.py

--- martin.py
+++ martin.py
@@ -30,7 +30,7 @@ class MARTIN:
     # формула Вильсона
     def calc_K(self):
         self.K = (np.exp(5.373 * (1 + self.omega) * (1 - self.T_cr / self.T)) * self.P_cr) / self.P
-
+    #TODO
     # вспомогательные коэффициенты
     def calc_omega_1(self):
         return 0.00756 + 0.90984 * self.omega + 0.1622 * self.omega ** 2 + 0.14549 * self.omega ** 3
@@ -46,7 +46,7 @@ class MARTIN:
 
     def calc_a_1(self):
         return -0.237 * self.T_r - 0.7846 / self.T_r + 1.0026 / self.T_r ** 2 + 0.019 / self.T_r ** 3
-
+    #добавить D TODO
     # параметры уравнения состояния
     def calc_a(self):
         self.a = 27 * self.R ** 2 * self.T_cr ** 2 * (64 * self.P_cr) * (
@@ -58,7 +58,7 @@ class MARTIN:
 
     def calc_b(self):
         self.b = (0.082 - 0.0713 * self.calc_omega_1()) * self.R * self.T_cr / self.P_cr
-
+    #добавить D TODO
     # решение Рашфорда-Райса
     def solve_RR(self):
         _, self.x, self.y = flash_inner_loop(self.z, self.K)
@@ -99,7 +99,7 @@ class MARTIN:
 
     def calc_Cm(self, flag: str):
         return self.calc_cm(flag) * self.P / (self.R * self.T)
-
+    #добавить D TODO
     # расчёты для компонентов
     def calc_Ai(self):
         return self.calc_a() * self.P / (self.R ** 2 * self.T ** 2)
@@ -109,10 +109,13 @@ class MARTIN:
 
     def calc_Ci(self):
         return self.calc_c() * self.P / (self.R * self.T)
+    #добавить D TODO
 
     def calc_fugacity_coeffs(self, flag):
         real_roots = []
+        #TODO 
         finding_z_factor = [1, self.calc_Am() - self.calc_Bm - 1, self.calc_Am() - self.calc_Bm() * self.calc_Cm() - self.calc_Cm(), self.calc_Am() * self.calc_Bm() ]
+        #TODO
         all_roots = np.roots(finding_z_factor)
 
         for value in all_roots:
@@ -133,11 +136,11 @@ class MARTIN:
         sum = 0
         for j in range(len(array)):
             sum += np.dot(array * matrix_a)
-
+        #TODO
         return np.exp(np.log(array * self.P) - np.log(Z - self.calc_Bm(flag)) \
                - (2 * sum / self.calc_am(flag) - self.calc_c() / self.calc_cm(flag)) * np.log(
             (Z + self.calc_Cm(flag)) / Z) * self.calc_am(flag) / (self.calc_Cm(flag)))
-
+        #TODO
     def launch_MartinE(self):
         while sum(np.abs(self.fugacity_l/self.fugacity_v - 1) > 10e-5)!=len(self.fugacity_l):
                 self.calc_a()
